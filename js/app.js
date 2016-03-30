024d7f9459f846c6ad8c3aad144b486b9b2434f2
benjclarke <bclarke@theshipyard.com>
2016-03-30 14:45:36 -0400

    updated speed

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
 
     this.x = x;
     this.y = y;
-    this.speed = Math.random() * 100;
+    var speed = Math.random() * 100;
+    this.speed = speed;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -29,10 +30,9 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function() {
+var Player = function(x, y) {
     this.x = x;
     this.y = y;
-    this.speed = speed;
     this.sprite = 'images/char-boy.png';
 
 };
@@ -43,19 +43,21 @@ Player.prototype.update = function() {
 };
 
 Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
 
 };
 
-Player.protoype.handleInput = function() {
+//Player.protoype.handleInput = function() {
 
 
-};
+//};
 
 var player = new Player(100, 400);
-var enemy1 = new Enemy(100, 75);
-var enemy2 = new Enemy(150, 150);
-var ememy3 = new Enemy(175, 200);
+
+var enemy1 = new Enemy(100, 300);
+var enemy2 = new Enemy(200, 220);
+var enemy3 = new Enemy(80, 60);
 
 
 // Now instantiate your objects.
